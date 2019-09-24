94e41319c7afc1241de5afb3d794d82c8e378659
Manish Karki <manishkarki@USMBP-mkarki.local>
2019-09-24 19:30:26 -0400

    added a module to calculate the density of a given binary tree, also added a positive test for the same

--- src/main/java/com/algo/tree/BinaryTree.java
+++ src/main/java/com/algo/tree/BinaryTree.java
@@ -27,7 +27,7 @@ public class BinaryTree {
      */
     public static int getHeight(Node root) {
         //using recursion
-        // if the element itself is null, it doesn't count to the height, base condition
+        //if the element itself is null, it doesn't count to the height, base condition
         if (root == null) {
             return 0;
         }
@@ -39,31 +39,76 @@ public class BinaryTree {
      * Height of a Binary Tree is a number of edges between the tree's root and its furthest leaf
      */
     public static int getHeightUsingIteration(Node root) {
-        Queue<Node> q = new LinkedList<>();
-        //add the root element
+        // Base Case
+        if (root == null)
+            return 0;
+
+        // Create an empty queue for level order traversal
+        Queue<Node> q = new LinkedList();
+
+        // Enqueue Root and initialize height
         q.add(root);
-        //add the delimiter for each level
+        int height = 0;
+
+        while (true) {
+            // nodeCount (queue size) indicates number of nodes
+            // at current level.
+            int nodeCount = q.size();
+            if (nodeCount == 0)
+                return height;
+            height++;
+
+            // Dequeue all nodes of current level and Enqueue all
+            // nodes of next level
+            while (nodeCount > 0) {
+                Node t = q.peek();
+                q.remove();
+                if (t.left != null)
+                    q.add(t.left);
+                if (t.right != null)
+                    q.add(t.right);
+                nodeCount--;
+            }
+        }
+    }
+
+    /**
+     * Given a Binary Tree, find the density of it by doing one traversal of it.
+     * <p>
+     * The density of binary tree is defined as:
+     * Density of Binary Tree = Size / Height
+     */
+    public float density(Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        //add a null delimiter
         q.add(null);
-        int height = 1;
+        int height = 1, size = 0;
         while (q.size() > 0) {
-            Node polled = q.poll();
-            //if it's at the same level, then just add left and right elements
-            if (polled != null) {
-                if(polled.left != null) {
-                    q.add(polled.left);
+            Node t = q.poll();
+
+            if (t != null) {
+                size++;
+                // add left and right elements, if present
+                if(t.left != null) {
+                    q.add(t.left);
                 }
-                if(polled.right != null) {
-                    q.add(polled.right);
+                if(t.right != null) {
+                    q.add(t.right);
                 }
+            } else {
+                //if there's no element remaining, then we've got final height and size
+                if (q.size() == 0) {
+                    break;
+                }
+                //else we'll add delimiter to the next level and increment the height
+                q.add(null);
+                height++;
                 continue;
             }
-            //if there's no element left after polling the null element, then we have reached the leaf or the bottom level
-            if (q.size() == 0)
-                return height;
-            q.add(null);
-            height++;
+
         }
-        return height;
+        return ((float) size) / height;
     }
 
 }
--- src/test/java/com/algo/tree/BinaryTreeTest.java
+++ src/test/java/com/algo/tree/BinaryTreeTest.java
@@ -15,27 +15,30 @@ class BinaryTreeTest {
     @BeforeEach
     public void initialize() {
         binaryTree = new BinaryTree();
+        setup(binaryTree);
     }
 
-    @Test
-    void testGetHeightUsingRecursion() {
+    private void setup(BinaryTree binaryTree) {
         binaryTree.root = newNode(1);
         binaryTree.root.left = newNode(2);
         binaryTree.root.right = newNode(3);
         binaryTree.root.left.left = newNode(4);
         binaryTree.root.left.right = newNode(5);
+    }
 
+    @Test
+    void testGetHeightUsingRecursion() {
         assertEquals(3, binaryTree.getHeight(binaryTree.root));
     }
 
     @Test
     void testGetHeightUsingIteration() {
-        binaryTree.root = newNode(1);
-        binaryTree.root.left = newNode(2);
-        binaryTree.root.right = newNode(3);
-        binaryTree.root.left.left = newNode(4);
-        binaryTree.root.left.right = newNode(5);
-
         assertEquals(3, binaryTree.getHeightUsingIteration(binaryTree.root));
+    }
+
+    @Test
+    void testGetDensity() {
+        float expected = 5f / 3;
+        assertEquals(expected, binaryTree.density(binaryTree.root));
     }
 }
