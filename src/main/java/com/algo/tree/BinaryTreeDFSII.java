f118b68b06b9f872410325dfde32b95995ba66b1
Manish Karki <8341591+manishkarki@users.noreply.github.com>
2019-09-26 21:46:17 -0400

    solved level order traversal

--- src/main/java/com/algo/tree/BinaryTreeDFSII.java
+++ src/main/java/com/algo/tree/BinaryTreeDFSII.java
@@ -1,7 +1,57 @@
 package com.algo.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @author mkarki
  */
-public class BinaryTreeDFSII {
+public class BinaryTreeDFSII extends BinaryTreeDFS {
+    /**
+     * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+     *
+     * For example:
+     * Given binary tree [3,9,20,null,null,15,7],
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     * return its level order traversal as:
+     * [
+     *   [3],
+     *   [9,20],
+     *   [15,7]
+     * ]
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) {
+            return new LinkedList<>();
+        }
+
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        List<List<Integer>> retList = new LinkedList<>();
+        nodeQueue.add(root);
+
+        while(!nodeQueue.isEmpty()) {
+            int size = nodeQueue.size();
+            List<Integer> interList = new LinkedList<>();
+            while(size > 0) {
+                TreeNode polled = nodeQueue.poll();
+                interList.add(polled.val);
+                if(polled.left != null) {
+                    nodeQueue.add(polled.left);
+                }
+                if(polled.right != null) {
+                    nodeQueue.add(polled.right);
+                }
+                size--;
+            }
+            retList.add(interList);
+        }
+        return retList;
+    }
 }
