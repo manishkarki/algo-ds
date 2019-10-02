0f0c1d3cd6dea79021d19f262355bd08a2fb966d
Manish Karki <manishkarki@USMBP-mkarki-2.local>
2019-10-01 22:15:07 -0400

    refactored the initial loops in the same

--- src/main/java/com/algo/leetcode/UniquePaths.java
+++ src/main/java/com/algo/leetcode/UniquePaths.java
@@ -42,15 +42,12 @@ public class UniquePaths {
         }
         int[][] dp = new int[m][n];
 
-        for(int i = 0; i<m;i++){
-            dp[i][0] = 1;
-        }
-        for(int j= 0;j<n;j++){
-            dp[0][j]=1;
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
