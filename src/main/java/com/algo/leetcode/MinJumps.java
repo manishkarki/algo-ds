87e0f8acb1cd595e796079401eb9c951a72286de
Manish Karki <mannishkarki@gmail.com>
2020-07-09 23:18:14 -0400

    reformatting

--- src/main/java/com/algo/leetcode/MinJumps.java
+++ src/main/java/com/algo/leetcode/MinJumps.java
@@ -9,11 +9,11 @@ public class MinJumps {
      * Given an array of integers where each element represents the max number of steps that can be
      * made forward from that element. Write a function to return the minimum number of jumps to reach the end of
      * the array (starting from the first element). If an element is 0, then we cannot move through that element
+     *
      * @param arr
      * @return minimum number of jumps required to reach the end
      */
-    public int minJumps(int arr[])
-    {
+    public int minJumps(int arr[]) {
         if (arr.length <= 1)
             return 0;
 
@@ -28,28 +28,26 @@ public class MinJumps {
 
 
         // Start traversing array
-        for (int i = 1; i < arr.length; i++)
-        {
+        for (int i = 1; i < arr.length; i++) {
             // Check if we have reached the end of the array
             if (i == arr.length - 1)
                 return jump;
 
             // updating maxReach
-            maxReach = Math.max(maxReach, i+arr[i]);
+            maxReach = Math.max(maxReach, i + arr[i]);
 
             // we use a step to get to the current index
             step--;
 
             // If no further steps left
-            if (step == 0)
-            {
+            if (step == 0) {
                 //  we must have used a jump
                 jump++;
 
                 // Check if the current index/position  or lesser index
                 // is the maximum reach point from the previous indexes
-                if(i>=maxReach) {
-                    System.out.println("can't do it anymore, since maxReach is:"+maxReach+" and i is:"+i);
+                if (i >= maxReach) {
+                    System.out.println("can't do it anymore, since maxReach is:" + maxReach + " and i is:" + i);
                     return -1;
                 }
 
@@ -62,9 +60,9 @@ public class MinJumps {
         return -1;
     }
 
-    boolean canReachEnd(int [] arr) {
+    boolean canReachEnd(int[] arr) {
         int maxReachSoFar = 0, lastIndex = arr.length - 1;
-        for(int i = 0; i <= maxReachSoFar && maxReachSoFar < lastIndex; i++) {
+        for (int i = 0; i <= maxReachSoFar && maxReachSoFar < lastIndex; i++) {
             maxReachSoFar = Math.max(maxReachSoFar, i + arr[i]);
         }
         return maxReachSoFar >= lastIndex;
@@ -72,7 +70,7 @@ public class MinJumps {
 
     public static void main(String[] args) {
         MinJumps mj = new MinJumps();
-        System.out.println(mj.minJumps(new int[]{2,2,1,1,0,2,3}));
-        System.out.println(mj.canReachEnd(new int[]{2,2,1,1,0,2,3}));
+        System.out.println(mj.minJumps(new int[]{2, 2, 1, 1, 0, 2, 3}));
+        System.out.println(mj.canReachEnd(new int[]{2, 2, 1, 1, 0, 2, 3}));
     }
 }
