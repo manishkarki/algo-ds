cf7a3276fe36d4bf1021c5034cab25a1d2407571
Manish Karki <mannishkarki@gmail.com>
2020-07-13 23:03:52 -0400

    wrote a module moveZeroesToTheEnd

--- src/main/java/com/algo/leetcode/thirtydaychallenge/AprilOneToSeven.java
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/AprilOneToSeven.java
@@ -103,4 +103,34 @@ public class AprilOneToSeven {
         return maxSubArraySum;
     }
 
+    /**
+     * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+     * <p>
+     * Example:
+     * <p>
+     * Input: [0,1,0,3,12]
+     * Output: [1,3,12,0,0]
+     * Note:
+     * <p>
+     * You must do this in-place without making a copy of the array.
+     * Minimize the total number of operations.
+     */
+    public void moveZeroesToTheEnd(int[] nums) {
+        if (nums == null || nums.length <= 0) {
+            System.out.println("there's nothing to do, exiting");
+            return;
+        }
+
+        int countNonZero = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[countNonZero++] = num;
+            }
+        }
+
+        while (countNonZero < nums.length) {
+            nums[countNonZero++] = 0;
+        }
+    }
+
 }
