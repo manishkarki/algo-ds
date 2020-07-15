14fa124ee02ae490835da45fd5980dbb68ae904d
Manish Karki <mannishkarki@gmail.com>
2020-07-14 22:47:58 -0400

    added the util class ListNode

---
+++ src/main/java/com/algo/util/ListNode.java
@@ -0,0 +1,21 @@
+package com.algo.util;
+
+/**
+ * @author mkarki
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
