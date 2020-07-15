302641bee4fd91fc2fd5699364794f788fa0c627
Manish Karki <mannishkarki@gmail.com>
2020-07-14 23:42:05 -0400

    added printList, equals and hashcode methods

--- src/main/java/com/algo/util/ListNode.java
+++ src/main/java/com/algo/util/ListNode.java
@@ -1,5 +1,7 @@
 package com.algo.util;
 
+import java.util.Objects;
+
 /**
  * @author mkarki
  */
@@ -19,11 +21,32 @@ public class ListNode {
         this.next = next;
     }
 
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.println("NULL");
+    }
+
     @Override
     public String toString() {
         return "ListNode{" +
                 "val=" + val +
                 ", next=" + next +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == null) {
+            return o == null;
+        }
+        return this.toString().equals(o.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
     }
 }
