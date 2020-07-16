f59b58549dd87b3c401ff3c00eed718c5b7c0d58
Manish Karki <mannishkarki@gmail.com>
2020-07-15 23:29:52 -0400

    wrote test for validParentheses

--- src/test/java/com/algo/leetcode/BalancedParenthesesTest.java
+++ src/test/java/com/algo/leetcode/BalancedParenthesesTest.java
@@ -1,8 +1,31 @@
 package com.algo.leetcode;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * @author mkarki
  */
 class BalancedParenthesesTest {
+
+    private BalancedParentheses balancedParentheses;
+
+    @BeforeEach
+    void setup() {
+        balancedParentheses = new BalancedParentheses();
+    }
+
+    @Test
+    void testValidParenthesesWithEmptyString() {
+        assertThat(balancedParentheses.isValid(""), is(true));
+    }
+
+    @Test
+    void testValidParenthesesWithNull() {
+        assertThat(balancedParentheses.isValid(null), is(true));
+    }
 
 }
