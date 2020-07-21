5647c554647cf43692f8db79d0b9a2f57ef04e6a
Manish Karki <mannishkarki@gmail.com>
2020-07-20 21:59:53 -0400

    implemented api for maxLength of a Contiguous subarray with equal 0s and 1s

---
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/april/eighttofourteen/ContiguousArray.java
@@ -0,0 +1,45 @@
+package com.algo.leetcode.thirtydaychallenge.april.eighttofourteen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author mkarki
+ */
+public class ContiguousArray {
+
+    /**
+     * Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+     * <p>
+     * Example 1:
+     * Input: [0,1]
+     * Output: 2
+     * Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+     * Example 2:
+     * Input: [0,1,0]
+     * Output: 2
+     * Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+     * Note: The length of the given binary array will not exceed 50,000.
+     *
+     * @param nums
+     * @return
+     */
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> countIndexes = new HashMap<>();
+        int maxlen = 0, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = sum + (nums[i] == 1 ? 1 : -1);
+            if (sum == 0) {
+                maxlen = i + 1;
+            }
+            if (countIndexes.containsKey(sum)) {
+                maxlen = Math.max(maxlen, i - countIndexes.get(sum));
+            } else {
+                countIndexes.put(sum, i);
+            }
+        }
+        return maxlen;
+    }
+}
+
+
