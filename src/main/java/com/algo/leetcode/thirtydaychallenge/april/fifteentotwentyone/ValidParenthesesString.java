5872fd82d803ec153b55218ab410696e79e80ff0
Manish Karki <mannishkarki@gmail.com>
2020-07-27 20:22:59 -0400

    IMPLEMENTED api for valid string

--- src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/ValidParenthesesString.java
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/ValidParenthesesString.java
@@ -5,6 +5,8 @@ package com.algo.leetcode.thirtydaychallenge.april.fifteentotwentyone;
  */
 public class ValidParenthesesString {
 
+    private static final Character[] combinations = new Character[]{'(', ')'};
+
     /**
      * Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
      * <p>
@@ -26,7 +28,30 @@ public class ValidParenthesesString {
      * The string size will be in the range [1, 100].
      */
     public boolean checkValidString(String s) {
+        int leftBalance = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) == '(') || (s.charAt(i) == '*'))
+                leftBalance++;
+            else
+                leftBalance--;
 
+            if (leftBalance < 0) return false;
+        }
+
+        if (leftBalance == 0) {
+            return true;
+        }
+
+        int rightBalance = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if ((s.charAt(i) == ')') || (s.charAt(i) == '*'))
+                rightBalance++;
+            else
+                rightBalance--;
+
+            if (rightBalance < 0) return false;
+        }
+        return true;
     }
 
 }
