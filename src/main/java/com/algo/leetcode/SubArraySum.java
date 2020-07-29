47c69e46f98d104d42dc7085db091a2af41fdfde
Manish Karki <mannishkarki@gmail.com>
2020-07-29 19:41:28 -0400

    specs noted

---
+++ src/main/java/com/algo/leetcode/SubArraySum.java
@@ -0,0 +1,27 @@
+package com.algo.leetcode;
+
+/**
+ * @author mkarki
+ */
+public class SubArraySum {
+
+    /**
+     * Given an unsorted array of integers, find a subarray which adds to a given number. If there are more than one subarrays with the sum as the given number, print any of them.
+     *
+     * Examples:
+     *
+     * Input: arr[] = {1, 4, 20, 3, 10, 5}, sum = 33
+     * Output: Sum found between indexes 2 and 4
+     * Explanation: Sum of elements between indices
+     * 2 and 4 is 20 + 3 + 10 = 33
+     *
+     * Input: arr[] = {10, 2, -2, -20, 10}, sum = -10
+     * Output: Sum found between indexes 0 to 3
+     * Explanation: Sum of elements between indices
+     * 0 and 3 is 10 + 2 - 2 - 20 = -10
+     *
+     * Input: arr[] = {-10, 0, 2, -2, -20, 10}, sum = 20
+     * Output: No subarray with given sum exists
+     * Explanation: There is no subarray with the given sum
+     */
+}
