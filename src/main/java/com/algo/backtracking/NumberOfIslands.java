0bcddf04ba1e52c48063866365c2efcf170d7620
Manish Karki <mannishkarki@gmail.com>
2020-07-29 21:07:16 -0400

    formatting

--- src/main/java/com/algo/backtracking/NumberOfIslands.java
+++ src/main/java/com/algo/backtracking/NumberOfIslands.java
@@ -8,21 +8,22 @@ public class NumberOfIslands {
     /**
      * Given a boolean 2D matrix, find the number of islands. A group of connected 1s forms an island.
      * For example, the below matrix contains 5 islands
-     *
+     * <p>
      * Example:
-     *
+     * <p>
      * Input : mat[][] = {{1, 1, 0, 0, 0},
-     *                    {0, 1, 0, 0, 1},
-     *                    {1, 0, 0, 1, 1},
-     *                    {0, 0, 0, 0, 0},
-     *                    {1, 0, 1, 0, 1}
+     * {0, 1, 0, 0, 1},
+     * {1, 0, 0, 1, 1},
+     * {0, 0, 0, 0, 0},
+     * {1, 0, 1, 0, 1}
      * Output : 5
+     *
      * @param mat
      * @return
      */
     public int getNumberOfIslands(int[][] mat) {
         //if matrix doesn't exist
-        if(mat == null) {
+        if (mat == null) {
             return 0;
         }
         int row = mat.length;
@@ -31,10 +32,10 @@ public class NumberOfIslands {
         boolean[][] visited = new boolean[row][col];
 
         int count = 0;
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 //if the cell hasn't been visited before but can be visited do the DFS on that
-                if(mat[i][j] == 1 && !visited[i][j]) {
+                if (mat[i][j] == 1 && !visited[i][j]) {
                     dfs(mat, i, j, visited);
                     //after DFS all the cells within the proximity has been visited and wouldn't count more than once
                     //so reference it as one island
@@ -52,8 +53,8 @@ public class NumberOfIslands {
 
         visited[i][j] = true;
 
-        for(int k = 0; k < 4; k++) {
-            if(isSafe(mat, i + xAxes[k], j + yAxes[k], visited)) {
+        for (int k = 0; k < 4; k++) {
+            if (isSafe(mat, i + xAxes[k], j + yAxes[k], visited)) {
                 dfs(mat, i + xAxes[k], j + yAxes[k], visited);
             }
         }
@@ -61,6 +62,6 @@ public class NumberOfIslands {
 
     private boolean isSafe(int[][] mat, int i, int j, boolean[][] visited) {
         //check if the cell is land
-        return i >= 0 && i < mat.length && j >=0 && j < mat[i].length && mat[i][j] ==1 && !visited[i][j];
+        return i >= 0 && i < mat.length && j >= 0 && j < mat[i].length && mat[i][j] == 1 && !visited[i][j];
     }
 }
