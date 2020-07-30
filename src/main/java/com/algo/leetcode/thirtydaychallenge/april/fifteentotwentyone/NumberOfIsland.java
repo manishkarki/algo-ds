456f403f8f08e8c6bfc35f98e1c2d337781961b0
Manish Karki <mannishkarki@gmail.com>
2020-07-29 20:31:34 -0400

    specification noted

---
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/NumberOfIsland.java
@@ -0,0 +1,35 @@
+package com.algo.leetcode.thirtydaychallenge.april.fifteentotwentyone;
+
+/**
+ * this question is available here
+ * https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/530/week-3/3302/
+ *
+ * @author mkarki
+ */
+public class NumberOfIsland {
+
+    /**
+     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: grid = [
+     *   ["1","1","1","1","0"],
+     *   ["1","1","0","1","0"],
+     *   ["1","1","0","0","0"],
+     *   ["0","0","0","0","0"]
+     * ]
+     * Output: 1
+     * Example 2:
+     *
+     * Input: grid = [
+     *   ["1","1","0","0","0"],
+     *   ["1","1","0","0","0"],
+     *   ["0","0","1","0","0"],
+     *   ["0","0","0","1","1"]
+     * ]
+     * Output: 3
+     */
+}
