a17d0a0607274838fd9ab2d9b24e74353b8790a5
Manish Karki <mannishkarki@gmail.com>
2020-07-29 21:07:38 -0400

    added coverage for positive test

---
+++ src/test/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/NumberOfIslandTest.java
@@ -0,0 +1,27 @@
+package com.algo.leetcode.thirtydaychallenge.april.fifteentotwentyone;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author mkarki
+ */
+public class NumberOfIslandTest {
+    private NumberOfIsland numberOfIsland;
+
+    @BeforeEach
+    void setup() {
+        numberOfIsland = new NumberOfIsland();
+    }
+
+    @Test
+    void testNumberOfIsland() {
+        assertThat(numberOfIsland.getIslandsNum(new char[][]{new char[]{'1', '1', '1', '1', '0'},
+                new char[]{'1', '1', '0', '1', '0'},
+                new char[]{'1', '1', '0', '0', '0'},
+                new char[]{'0', '0', '0', '0', '0'}}), is(1));
+    }
+}
