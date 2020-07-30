980fe4b09be707a9ee5fa623982d4bfc9c19aef9
Manish Karki <mannishkarki@gmail.com>
2020-07-29 21:17:57 -0400

    added safety for null and empty inputs

--- src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/NumberOfIsland.java
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/NumberOfIsland.java
@@ -33,11 +33,11 @@ public class NumberOfIsland {
      * Output: 3
      */
     public int getIslandsNum(char[][] grid) {
-        if (grid == null) {
+        if (grid == null || grid.length == 0) {
             return 0;
         }
         int islandCount = 0;
-        int row = grid.length, col = grid[0].length;
+        int row = grid.length, col = grid[0] != null ? grid[0].length : 0;
         int[][] visited = new int[row][col];
 
         for (int i = 0; i < row; i++) {
