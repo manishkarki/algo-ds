198cc36ab9fbf8abd169e67ee43a7d2f7962c8a9
Manish Karki <mannishkarki@gmail.com>
2020-08-03 20:46:31 -0400

    added the method implementation for RotatedArraySearch using modified BinarySearch

--- src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/RotatedArraySearch.java
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/RotatedArraySearch.java
@@ -24,4 +24,37 @@ public class RotatedArraySearch {
      * Input: nums = [4,5,6,7,0,1,2], target = 3
      * Output: -1
      */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        return search(nums, 0, nums.length - 1, target);
+    }
+
+    private int search(int[] nums, int low, int high, int target) {
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // if first subArray is sorted
+        if (nums[low] <= nums[mid]) {
+            if (target >= nums[low] && target <= nums[mid]) {
+                return search(nums, low, mid - 1, target);
+            }
+            return search(nums, mid + 1, high, target);
+        }
+
+        // if first subArray(nums[low..mid] is not sorted then nums[mid+1...high] must be sorted
+        if (target >= nums[mid] && target <= nums[high]) {
+            return search(nums, mid + 1, high, target);
+        }
+
+        return search(nums, low, mid - 1, target);
+    }
 }
