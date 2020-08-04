97c4701ea900dfdf13be44486cb9c86d8d853028
Manish Karki <mannishkarki@gmail.com>
2020-08-03 20:50:25 -0400

    added tests for invalid inputs also added tests for not available target

--- src/test/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/RotatedArraySearchTest.java
+++ src/test/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/RotatedArraySearchTest.java
@@ -18,9 +18,20 @@ class RotatedArraySearchTest {
     }
 
     @Test
+    void testSearchWithInvalidInputs() {
+        assertThat(rotatedArraySearch.search(null, 10), is(-1));
+        assertThat(rotatedArraySearch.search(new int[]{}, 10), is(-1));
+    }
+
+    @Test
     void testSearchWithValidInput() {
         assertThat(rotatedArraySearch.search(new int[]{5, 6, 7, 1, 2, 3, 4}, 3), is(5));
         assertThat(rotatedArraySearch.search(new int[]{5, 6, 7, 1, 2, 3, 4}, 4), is(6));
         assertThat(rotatedArraySearch.search(new int[]{5, 6, 7, 1, 2, 3, 4}, 5), is(0));
+    }
+
+    @Test
+    void testSearchWithUnavailable() {
+        assertThat(rotatedArraySearch.search(new int[]{5, 6, 7, 1, 2, 3, 4}, 10), is(-1));
     }
 }
