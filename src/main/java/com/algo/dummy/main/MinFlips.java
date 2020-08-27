7bc1fb725162e081a6ec835a853f5eea13c72be3
Manish Karki <mannishkarki@gmail.com>
2020-08-26 21:41:54 -0400

    writing down the specs

---
+++ src/main/java/com/algo/dummy/main/MinFlips.java
@@ -0,0 +1,22 @@
+package com.algo.dummy.main;
+
+/**
+ * @author mkarki
+ */
+public class MinFlips {
+    /**
+     * There are N coins, each showing either heads or tails. We would like all the coins to form a sequence
+     * of alternating heads and tails. What is the minimum number of coins that must be reversed to achieve this?
+     * e.g:
+     * Given Array A = [1, 0, 1, 0, 1, 1], it should return 1
+     * Given Array A = [1, 1, 0, 1, 1], it should return 2
+     * Given Array A = [0, 1, 0], it should return 0
+     *
+     * @param A
+     * @return
+     */
+    public int solution(int[] A) {
+    }
+
+
+}
