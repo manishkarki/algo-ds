007e9b825cf9f1539a6819cc62081a064ce4f830
Manish Karki <mannishkarki@gmail.com>
2020-08-26 21:42:27 -0400

    solution

--- src/main/java/com/algo/dummy/main/MinFlips.java
+++ src/main/java/com/algo/dummy/main/MinFlips.java
@@ -16,6 +16,21 @@ public class MinFlips {
      * @return
      */
     public int solution(int[] A) {
+        System.out.println(getNumberOfReverse(A, 0));
+        System.out.println(getNumberOfReverse(A, 1));
+        return Math.min(getNumberOfReverse(A, 0), getNumberOfReverse(A, 1));
+    }
+
+    public static int getNumberOfReverse(int[] A, int target) {
+        int countFlip = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != target) {
+                countFlip++;
+            }
+            // flip for next iteration
+            target = (target == 0) ? 1 : 0;
+        }
+        return countFlip;
     }
 
 
