35dbaef0542a05779ba65b19444d58936d20f786
Manish Karki <mannishkarki@gmail.com>
2021-02-11 21:57:39 -0500

    specs

---
+++ src/main/java/com/algo/arrays/MergeSortedArrays.java
@@ -0,0 +1,31 @@
+package com.algo.arrays;
+
+/**
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ * <p>
+ * The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * Example 2:
+ * <p>
+ * Input: nums1 = [1], m = 1, nums2 = [], n = 0
+ * Output: [1]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -109 <= nums1[i], nums2[i] <= 109
+ *
+ * @author mkarki
+ */
+public class MergeSortedArrays {
+}
