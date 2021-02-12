d74cecb7085ce9207160789d342454d4ef568aed
Manish Karki <mannishkarki@gmail.com>
2021-02-11 21:57:14 -0500

    specs

--- src/main/java/com/algo/arrays/DuplicateZeroes.java
+++ src/main/java/com/algo/arrays/DuplicateZeroes.java
@@ -1,6 +1,31 @@
 package com.algo.arrays;
 
 /**
+ * Given a fixed length array arr of integers, duplicate each occurrence of zero, shifting the remaining elements to the right.
+ * <p>
+ * Note that elements beyond the length of the original array are not written.
+ * <p>
+ * Do the above modifications to the input array in place, do not return anything from your function.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,0,2,3,0,4,5,0]
+ * Output: null
+ * Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+ * Example 2:
+ * <p>
+ * Input: [1,2,3]
+ * Output: null
+ * Explanation: After calling your function, the input array is modified to: [1,2,3]
+ * <p>
+ * <p>
+ * Note:
+ * <p>
+ * 1 <= arr.length <= 10000
+ * 0 <= arr[i] <= 9
+ *
  * @author mkarki
  */
 public class DuplicateZeroes {
