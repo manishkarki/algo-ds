03af10880f68b444cfb2b8dd4e018aedfb193f45
Manish Karki <mannishkarki@gmail.com>
2021-02-11 21:56:41 -0500

    gt vs lt

--- src/main/java/com/algo/arrays/DuplicateZeroes.java
+++ src/main/java/com/algo/arrays/DuplicateZeroes.java
@@ -9,7 +9,7 @@ public class DuplicateZeroes {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 // do the shift to the right
-                for (int j = arr.length - 1; j < i; j--) {
+                for (int j = arr.length - 1; j > i; j--) {
                     arr[j] = arr[j - 1];
                 }
                 i++;
