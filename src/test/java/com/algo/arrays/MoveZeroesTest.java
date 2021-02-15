88e9c61bb75ce78120638a7313adfd7b8d79fff7
Manish Karki <mannishkarki@gmail.com>
2021-02-14 23:01:33 -0500

    simple test without assertion

--- src/test/java/com/algo/arrays/MoveZeroesTest.java
+++ src/test/java/com/algo/arrays/MoveZeroesTest.java
@@ -1,8 +1,21 @@
 package com.algo.arrays;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 /**
  * @author mkarki
  */
 public class MoveZeroesTest {
+
+    @Test
+    void testMoveZeroesToTheEnd() {
+        int[] nums = {1, 0, 3, 4, 0, 5, 0, 0, 8, 9, 10};
+
+        MoveZeroes.moveZeroes(nums);
+        Arrays.stream(nums)
+                .forEach(i -> System.out.print(" " + i));
+    }
 
 }
