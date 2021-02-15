96b587e4e08d683a662f22eb974fb6f978a3b73e
Manish Karki <mannishkarki@gmail.com>
2021-02-14 21:44:54 -0500

    test for the replaceElement

--- src/test/java/com/algo/arrays/ReplaceElementTest.java
+++ src/test/java/com/algo/arrays/ReplaceElementTest.java
@@ -1,8 +1,18 @@
 package com.algo.arrays;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * @author mkarki
  */
 class ReplaceElementTest {
+
+    @Test
+    void testReplaceElementsOnRight() {
+        assertThat(ReplaceElement.replaceElements(new int[]{17, 18, 5, 4, 6, 1}), is(new int[]{18, 6, 6, 6, 1, -1}));
+    }
 
 }
