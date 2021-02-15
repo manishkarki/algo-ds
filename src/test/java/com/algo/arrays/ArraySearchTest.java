43dd018c4c8d343de02ccc4aceed6d6ec9637790
Manish Karki <mannishkarki@gmail.com>
2021-02-14 19:00:22 -0500

    test cases for methodII

--- src/test/java/com/algo/arrays/ArraySearchTest.java
+++ src/test/java/com/algo/arrays/ArraySearchTest.java
@@ -12,8 +12,15 @@ public class ArraySearchTest {
 
     @Test
     void testElement() {
-//        assertThat(ArraySearch.checkIfExist(new int[] {3, 1, 7, 11}), is(false));
+        assertThat(ArraySearch.checkIfExist(new int[]{3, 1, 7, 11}), is(false));
         assertThat(ArraySearch.checkIfExist(new int[]{-2, 0, 10, -19, 4, 6, -8}), is(false));
         assertThat(ArraySearch.checkIfExist(new int[]{0, 0}), is(true));
+    }
+
+    @Test
+    void testElementAndItsDoubleExistsInAnArray() {
+        assertThat(ArraySearch.checkIfExists(new int[]{3, 1, 7, 11}), is(false));
+        assertThat(ArraySearch.checkIfExists(new int[]{-2, 0, 10, -19, 4, 6, -8}), is(false));
+        assertThat(ArraySearch.checkIfExists(new int[]{0, 0}), is(true));
     }
 }
