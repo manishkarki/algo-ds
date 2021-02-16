deaa7364191fd9476f8dc2a26c79905543eb02b2
Manish Karki <mannishkarki@gmail.com>
2021-02-15 21:39:54 -0500

    test for Disappeared numbers in an array

---
+++ src/test/java/com/algo/arrays/DisappearedNumberTest.java
@@ -0,0 +1,17 @@
+package com.algo.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+/**
+ * @author mkarki
+ */
+class DisappearedNumberTest {
+
+    @Test
+    void testGetDisappearedNumbers() {
+        assertThat(DisappearedNumber.getDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}), containsInAnyOrder(5, 6));
+    }
+}
