d13d2684811bcc8218aa17f10ec48168f3be5d6c
Manish Karki <mannishkarki@gmail.com>
2021-02-25 19:46:11 -0500

    specs

---
+++ src/main/java/com/algo/arrays/LargestSubArray.java
@@ -0,0 +1,20 @@
+package com.algo.arrays;
+
+/**
+ * Given an array containing only 0s and 1s, find the largest subarray which contains equal no of 0s and 1s. The expected time complexity is O(n).
+ * Examples:
+ * <p>
+ * Input: arr[] = {1, 0, 1, 1, 1, 0, 0}
+ * Output: 1 to 6
+ * (Starting and Ending indexes of output subarray)
+ * <p>
+ * Input: arr[] = {1, 1, 1, 1}
+ * Output: No such subarray
+ * <p>
+ * Input: arr[] = {0, 0, 1, 1, 0}
+ * Output: 0 to 3 Or 1 to 4
+ *
+ * @author mkarki
+ */
+public class LargestSubArray {
+}
