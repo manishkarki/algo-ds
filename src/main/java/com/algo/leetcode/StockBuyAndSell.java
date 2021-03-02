8fd1d7e046b7ff8874eb2d2ad97fc930307af6da
Manish Karki <mannishkarki@gmail.com>
2021-03-01 21:24:23 -0500

    making the method static

--- src/main/java/com/algo/leetcode/StockBuyAndSell.java
+++ src/main/java/com/algo/leetcode/StockBuyAndSell.java
@@ -26,16 +26,16 @@ public class StockBuyAndSell {
      * @param prices
      * @return
      */
-    public int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices) {
 
-        if(prices == null || prices.length == 0) {
+        if (prices == null || prices.length == 0) {
             return 0;
         }
 
         int maxProfit = 0;
         int minPrice = Integer.MAX_VALUE;
 
-        for(int i = 0; i < prices.length; i++) {
+        for (int i = 0; i < prices.length; i++) {
             maxProfit = Math.max(maxProfit, prices[i] - minPrice);
             minPrice = Math.min(minPrice, prices[i]);
         }
