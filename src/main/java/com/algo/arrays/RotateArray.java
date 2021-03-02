700b3fcf2b68cd6da11c73fb1afa07c63621824e
Manish Karki <mannishkarki@gmail.com>
2021-03-01 21:45:33 -0500

    specs for rotate array

---
+++ src/main/java/com/algo/arrays/RotateArray.java
@@ -0,0 +1,38 @@
+package com.algo.arrays;
+
+/**
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ * <p>
+ * Follow up:
+ * <p>
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,4,5,6,7], k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,-100,3,99], k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 2 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ *
+ * @author mkarki
+ */
+public class RotateArray {
+}
