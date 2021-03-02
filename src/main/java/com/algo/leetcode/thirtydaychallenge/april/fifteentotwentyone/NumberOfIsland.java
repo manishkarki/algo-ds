a60ee00aae0265aaf87a7ae454606bcbdbad9f07
Manish Karki <mannishkarki@gmail.com>
2021-03-01 21:29:05 -0500

    formatting

--- src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/NumberOfIsland.java
+++ src/main/java/com/algo/leetcode/thirtydaychallenge/april/fifteentotwentyone/NumberOfIsland.java
@@ -9,7 +9,9 @@ package com.algo.leetcode.thirtydaychallenge.april.fifteentotwentyone;
 public class NumberOfIsland {
 
     /**
-     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+     * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+     * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+     * You may assume all four edges of the grid are all surrounded by water.
      * <p>
      * <p>
      * <p>
