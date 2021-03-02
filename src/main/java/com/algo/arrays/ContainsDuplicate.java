bfe8fdbb53b97b60abe6acb480a001e66877fc99
Manish Karki <mannishkarki@gmail.com>
2021-03-01 22:32:42 -0500

    specs for containsduplicate

---
+++ src/main/java/com/algo/arrays/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package com.algo.arrays;
+
+/**
+ * Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ * Example 3:
+ * <p>
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
+ *
+ * @author mkarki
+ */
+public class ContainsDuplicate {
+}
