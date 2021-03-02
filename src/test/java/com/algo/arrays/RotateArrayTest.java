65d4c8fe1d3cbe265d0d8850953a0ef3f8b33dc7
Manish Karki <mannishkarki@gmail.com>
2021-03-01 22:17:50 -0500

    added test for solution II with extra space

--- src/test/java/com/algo/arrays/RotateArrayTest.java
+++ src/test/java/com/algo/arrays/RotateArrayTest.java
@@ -16,4 +16,11 @@ public class RotateArrayTest {
         RotateArray.rotateArray(nums, 3);
         assertThat(nums, is(new int[]{5, 6, 7, 1, 2, 3, 4}));
     }
+
+    @Test
+    void testRotateArrayII() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        RotateArray.rotateArrayII(nums, 3);
+        assertThat(nums, is(new int[]{5, 6, 7, 1, 2, 3, 4}));
+    }
 }
