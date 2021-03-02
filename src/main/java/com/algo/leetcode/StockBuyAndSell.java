3f21fd04ceeeef115cb7b8d1349430e72c600ee0
Manish Karki <mannishkarki@gmail.com>
2021-03-01 21:30:11 -0500

    solution to stock buy and sell with multiple transactions allowed

--- src/main/java/com/algo/leetcode/StockBuyAndSell.java
+++ src/main/java/com/algo/leetcode/StockBuyAndSell.java
@@ -77,6 +77,13 @@ public class StockBuyAndSell {
      * 0 <= prices[i] <= 104
      */
     public static int getMaxProfitFromStockBuySellII(int[] prices) {
+        int maxProfit = 0;
 
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return maxProfit;
     }
 }
