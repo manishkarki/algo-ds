55d14bd97ec1cecf7e80a25ba62ebf08de2b5b12
Manish Karki <mannishkarki@gmail.com>
2021-03-02 23:15:44 -0500

    added test for plus one

---
+++ src/test/java/com/algo/arrays/PlusOneTest.java
@@ -0,0 +1,18 @@
+package com.algo.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * @author mkarki
+ */
+class PlusOneTest {
+
+    @Test
+    void testPlusOne() {
+        int[] numbers = {1, 2, 9};
+        assertThat(PlusOne.plusOne(numbers), is(new int[]{1, 3, 0}));
+    }
+}
