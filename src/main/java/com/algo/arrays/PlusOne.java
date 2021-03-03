90dffe491d607d9ba21952663509a765b92ef418
Manish Karki <mannishkarki@gmail.com>
2021-03-02 23:10:20 -0500

    specs for plusOne problem

---
+++ src/main/java/com/algo/arrays/PlusOne.java
@@ -0,0 +1,30 @@
+package com.algo.arrays;
+
+/**
+ * Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+ * <p>
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contains a single digit.
+ * <p>
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: digits = [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * Example 2:
+ * <p>
+ * Input: digits = [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * Example 3:
+ * <p>
+ * Input: digits = [0]
+ * Output: [1]
+ *
+ * @author mkarki
+ */
+public class PlusOne {
+}
