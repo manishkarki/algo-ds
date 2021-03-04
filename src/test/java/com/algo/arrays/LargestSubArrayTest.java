a17bbdf5c2cb720092c114dc682a94ed29037179
Manish Karki <mannishkarki@gmail.com>
2021-03-03 20:37:09 -0500

    added assertion

--- src/test/java/com/algo/arrays/LargestSubArrayTest.java
+++ src/test/java/com/algo/arrays/LargestSubArrayTest.java
@@ -2,6 +2,7 @@ package com.algo.arrays;
 
 import org.junit.jupiter.api.Test;
 
+import static com.algo.arrays.LargestSubArray.getLargestSubArrayWithEqualAlphabetsAndNumbers;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -18,6 +19,15 @@ class LargestSubArrayTest {
         assertThat(LargestSubArray.getMaxLenWithEqualNumberOfZeroesAndOnes(arr), is(0));
         arr = new int[]{0, 0, 1, 1, 0};
         assertThat(LargestSubArray.getMaxLenWithEqualNumberOfZeroesAndOnes(arr), is(4));
+    }
+
+    @Test
+    void test() {
+        int[] arr = {'A', 'B', 'X', 4, 6, 'X', 'a'};
+        LargestSubArray.SubArrayResponse subArrayResponse = getLargestSubArrayWithEqualAlphabetsAndNumbers(arr);
+        System.out.println(subArrayResponse);
+        assertThat(subArrayResponse.indices, is(new int[]{1, 4}));
+        assertThat(subArrayResponse.maxSize, is(4));
     }
 
 }
