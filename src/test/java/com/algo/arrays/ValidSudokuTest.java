5ae201b4a6f42c4d8405c529f6f3984e3be7f0a9
Manish Karki <mannishkarki@gmail.com>
2021-03-03 19:46:09 -0500

    adding test for sudoku

--- src/test/java/com/algo/arrays/ValidSudokuTest.java
+++ src/test/java/com/algo/arrays/ValidSudokuTest.java
@@ -2,6 +2,9 @@ package com.algo.arrays;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 /**
  * @author mkarki
  */
@@ -9,6 +12,16 @@ class ValidSudokuTest {
 
     @Test
     void testIsValidSudoku() {
-
+        char[][] board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        assertThat(ValidSudoku.isValidSudoku(board), is(true));
     }
 }
