e3f2155318ccf4033fb61cb1f345f4003ad5dcc0
Manish Karki <mannishkarki@gmail.com>
2021-03-04 21:09:09 -0500

    sample

--- src/test/java/com/algo/arrays/SetMismatchTest.java
+++ src/test/java/com/algo/arrays/SetMismatchTest.java
@@ -15,4 +15,20 @@ class SetMismatchTest {
         assertThat(SetMismatch.findErrorNums(new int[]{1, 2, 2, 4}), is(new int[]{2, 3}));
         assertThat(SetMismatch.findErrorNums(new int[]{1, 1}), is(new int[]{1, 2}));
     }
+
+    @Test
+    void test() {
+        System.out.println(longestCommonPrefix(new String[]{"leets", "leetcode", "leet", "leeds"}));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
 }
