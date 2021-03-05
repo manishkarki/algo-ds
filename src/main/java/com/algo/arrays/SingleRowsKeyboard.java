852e0092b0f10e584d6f7cc499fbd9c89dcb1ea6
Manish Karki <mannishkarki@gmail.com>
2021-03-04 19:33:40 -0500

    missing number

--- src/main/java/com/algo/arrays/SingleRowsKeyboard.java
+++ src/main/java/com/algo/arrays/SingleRowsKeyboard.java
@@ -50,4 +50,19 @@ public class SingleRowsKeyboard {
         return time;
     }
 
+    public static int missingNumber(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return n * (n + 1) / 2 - sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(missingNumber(new int[]{0, 1, 3}));
+        System.out.println(missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+    }
+
 }
