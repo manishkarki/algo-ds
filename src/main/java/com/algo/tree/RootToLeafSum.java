e8400f9bf072971aa5b5c28ad26a78cd52c30eaf
Manish Karki <mannishkarki@gmail.com>
2021-03-07 23:37:38 -0500

    RootToLeafSum specification

---
+++ src/main/java/com/algo/tree/RootToLeafSum.java
@@ -0,0 +1,38 @@
+package com.algo.tree;
+
+/**
+ * You are given the root of a binary tree where each node has a value 0 or 1.
+ * <p>
+ * Each root-to-leaf path represents a binary number starting with the most significant bit.
+ * For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+ * <p>
+ * For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+ * <p>
+ * Return the sum of these numbers. The answer is guaranteed to fit in a 32-bits integer.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: root = [1,0,1,0,1,0,1]
+ * Output: 22
+ * Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+ * Example 2:
+ * <p>
+ * Input: root = [0]
+ * Output: 0
+ * Example 3:
+ * <p>
+ * Input: root = [1]
+ * Output: 1
+ * Example 4:
+ * <p>
+ * Input: root = [1,1]
+ * Output: 3
+ *
+ * @author mkarki
+ */
+public class RootToLeafSum {
+
+}
