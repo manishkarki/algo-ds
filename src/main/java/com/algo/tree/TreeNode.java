2c491d1afa41687f83849fa75aa83a402c699fcd
Manish Karki <mannishkarki@gmail.com>
2021-03-08 18:35:56 -0500

    refactored TreeNode to take object number, added helper to build tree from list and print the same

--- src/main/java/com/algo/tree/TreeNode.java
+++ src/main/java/com/algo/tree/TreeNode.java
@@ -1,20 +1,75 @@
 package com.algo.tree;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @author mkarki
  */
 public class TreeNode {
-    int val;
+    Integer val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
+    public TreeNode() {
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    TreeNode(Integer x) {
+        this.val = x;
+    }
+
+    TreeNode(Integer val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
     }
+
+    public TreeNode buildTreePreOrderFromList(List<Integer> nums) {
+
+        TreeNode root = new TreeNode(0);
+        root = insertLevelOrder(nums, root, 0);
+        printLevelOrder(root);
+        return root;
+    }
+
+    void printLevelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode tempNode = queue.poll();
+            System.out.print(tempNode.val + " ");
+
+            /*Enqueue left child */
+            if (tempNode.left != null) {
+                queue.add(tempNode.left);
+            }
+
+            /*Enqueue right child */
+            if (tempNode.right != null) {
+                queue.add(tempNode.right);
+            }
+        }
+    }
+
+
+    public TreeNode insertLevelOrder(List<Integer> nums, TreeNode root, int i) {
+
+        // Base case for recursion
+        if (i < nums.size()) {
+            if (nums == null || nums.get(i) == null) {
+                System.out.println("nums was null for i :" + i);
+            }
+            TreeNode temp = new TreeNode(nums.get(i));
+            root = temp;
+
+            // insert left child
+            root.left = insertLevelOrder(nums, root.left, 2 * i + 1);
+
+            // insert right child
+            root.right = insertLevelOrder(nums, root.right, 2 * i + 2);
+        }
+        return root;
+    }
+
 }
