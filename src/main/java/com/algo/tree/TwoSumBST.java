f6f394b2927038907ed4aa78f818d8045de6dd6f
Manish Karki <mannishkarki@gmail.com>
2021-03-08 17:04:16 -0500

    solution for TwoSumBST

--- src/main/java/com/algo/tree/TwoSumBST.java
+++ src/main/java/com/algo/tree/TwoSumBST.java
@@ -1,5 +1,8 @@
 package com.algo.tree;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Given the roots of two binary search trees, root1 and root2, return true if and only if there is a node in
  * the first tree and a node in the second tree whose values sum up to a given integer target.
@@ -21,5 +24,30 @@ package com.algo.tree;
  * @author mkarki
  */
 public class TwoSumBST {
+    public boolean twoSumBSTs(TreeNode root1, TreeNode root2, int target) {
+        TreeNode curr1 = root1;
+        TreeNode curr2 = root2;
+        // in order add values from curr1 to set
+        Set<Integer> curr1Values = new HashSet<>();
+        Set<Integer> curr2Values = new HashSet<>();
+        fillInOrder(curr1, curr1Values);
+        fillInOrder(curr2, curr2Values);
 
+        for (int currValue : curr1Values) {
+            if (curr2Values.contains(target - currValue)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void fillInOrder(TreeNode node, Set<Integer> values) {
+        if (node == null) {
+            return;
+        }
+        fillInOrder(node.left, values);
+        values.add(node.val);
+        fillInOrder(node.right, values);
+    }
 }
