ec3e5495a824334e9f8cfa9bd5d0c79fe3e8cc46
Manish Karki <mannishkarki@gmail.com>
2021-03-08 21:23:08 -0500

    specs for NumberOfSwaps and CountSmallerNumbersAfterSelf

---
+++ src/test/java/com/algo/sorting/NumberOfSwaps.java
@@ -0,0 +1,14 @@
+package com.algo.sorting;
+
+/**
+ * Given an array and a sorting algorithm, the sorting algorithm will do a selection swap.
+ * Find the number of swaps to sort the array.
+ * <p>
+ * Example 1:
+ * Input: [5, 4, 1, 2]
+ * Output: 5
+ *
+ * @author mkarki
+ */
+public class NumberOfSwaps {
+}
