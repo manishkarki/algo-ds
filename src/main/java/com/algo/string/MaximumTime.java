df9c2cb494f9f7278ac34cb53392c878cee64d41
Manish Karki <mannishkarki@gmail.com>
2021-03-10 00:00:30 -0500

    specs for maximum time

---
+++ src/main/java/com/algo/string/MaximumTime.java
@@ -0,0 +1,30 @@
+package com.algo.string;
+
+/**
+ * You are given a string time in the form of hh:mm, where some of the digits in the string are hidden (represented by ?).
+ * <p>
+ * The valid times are those inclusively between 00:00 and 23:59.
+ * <p>
+ * Return the latest valid time you can get from time by replacing the hidden digits.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: time = "2?:?0"
+ * Output: "23:50"
+ * Explanation: The latest hour beginning with the digit '2' is 23 and the latest minute ending with the digit '0' is 50.
+ * Example 2:
+ * <p>
+ * Input: time = "0?:3?"
+ * Output: "09:39"
+ * Example 3:
+ * <p>
+ * Input: time = "1?:22"
+ * Output: "19:22"
+ *
+ * @author mkarki
+ */
+public class MaximumTime {
+    
+}
